d6d2d86cce5f54b55517c5b37b3280058fe71120
Iago América <iago6489@gmail.com>
2021-11-09 13:35:08 -0300

    Start Commit

---
+++ Nubank_clone/app/src/main/java/com/example/nubank/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.nubank
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Chama a LoginActivity
+            val mIntent= Intent(this,MainActivity::class.java)
+            //método responsável por executar a Intent
+            startActivity(mIntent)
+            //método responsável por remover a tela da pilha
+            finish()
+        },3000)//tempo em milisegundos
+    }
+}
